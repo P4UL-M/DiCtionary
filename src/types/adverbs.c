bacc675aae695719fef0cd07cd3327619c586d3e
Quentin <quentin.cardona@efrei.net>
2022-10-18 18:46:48 +0200

    commentary

--- src/types/adverbs.c
+++ src/types/adverbs.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 p_tree_adv create_tree_adv(){
-    p_tree_adv tree = malloc(sizeof(tree));
+    p_tree_adv tree = malloc(sizeof(t_tree_adv));
     tree->children = malloc(sizeof(p_node_adv) * MAX);
     for (int i = 0; i < MAX; i++)
     {
@@ -14,13 +14,15 @@ p_tree_adv create_tree_adv(){
 }
 
 p_node_adv createnode_adv(char val){
-    p_node_adv n=malloc(sizeof(p_node_adv)*MAX);
-    n->val=val;
-    n->nb_child=MAX;
+    p_node_adv node=malloc(sizeof(t_node_adv));
+    node->val=val;
+    node->children = malloc(sizeof(p_node_adv) * MAX);
     for (int i = 0; i < MAX; i++)
     {
-        n->children[i] = NULL;
+        node->children[i] = NULL;
     }
+    node->nb_child=0;
+    return node;
 }
 
 void add_child_adv(p_node_adv parent, p_node_adv child)
