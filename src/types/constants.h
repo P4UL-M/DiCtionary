0f953430777e91c75c30939caca34b4ff3dbad89
P4UL-M <paul.mairesse@free.fr>
2022-10-27 09:43:46 +0200

    BIT FLAGS

--- src/types/constants.h
+++ src/types/constants.h
@@ -4,6 +4,7 @@ This file contains the constants and define*/
 #ifndef verb_h
 #define verb_h
 
+// string constants
 #define VERB_TYPE "Ver"
 #define NOUN_TYPE "Nom"
 #define ADJECTIVE_TYPE "Adj"
@@ -38,5 +39,29 @@ This file contains the constants and define*/
 #define ANSI_COLOR_CYAN "\x1b[0m\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 #define ANSI_BACKGROUND_WHITE "\x1b[0m\x1b[39;1m\e[47m\e[K"
+
+// binary constants
+#define SG_BIT b '0x1'
+#define PL_BIT b '0x2'
+
+#define P1_BIT b '0x4'
+#define P2_BIT b '0x8'
+#define P3_BIT b '0x10'
+
+#define IPre_BIT b '0x20'
+#define IImp_BIT b '0x40'
+#define SPre_BIT b '0x80'
+#define IPsimp_BIT b '0x100'
+#define PPre_BIT b '0x200'
+#define SImp_BIT b '0x400'
+#define PPas_BIT b 'Ox800'
+#define IFut_BIT b '0x1000'
+#define CPre_BIT b '0x2000'
+#define Inf_BIT b '0x4000'
+
+#define Mas_BIT b '0x4'
+#define Fem_BIT b '0x8'
+
+unsigned int get_bit(char *str);
 
 #endif
